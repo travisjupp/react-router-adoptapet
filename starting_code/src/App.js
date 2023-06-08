9fad020f3a4de368e54cf61f552e654169df8717
tjupp <travisjupp@gmail.com>
2023-06-08 15:40:14 -0600

    add PetDetailsNotFound route

--- starting_code/src/App.js
+++ starting_code/src/App.js
@@ -17,6 +17,7 @@ const appRouter = createBrowserRouter(
       <Route path=':type' element={<HomePage />} />
       <Route path=':type/:id' element={<PetDetailsPage />} />
       <Route path='/search' element={<SearchPage />} />
+      <Route path='/pet-details-not-found' element={<PetDetailsNotFound />} />
     </Route>
   )
 );
